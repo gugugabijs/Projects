672a431a5f03540215687e4dbc36aa77107c530e
gugugabijs <skyleradamsson@gmail.com>
2023-07-30 20:08:33 +0400

    added new homework

---
+++ mouseEvents/index.js
@@ -0,0 +1,9 @@
+function cardHover(item){
+   item.style.width="25%";
+   item.style.transition= "width 1s";
+}
+
+function cardLeave(item){
+    item.style.width="21%";
+    item.style.transition= "width 1s";
+}
